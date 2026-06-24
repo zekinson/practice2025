de3094c49d3de24e195079409207bf6bfe60689e
zekinson <zekagorhakov@gmail.com>
2026-06-24 19:59:49 +0600

    feat: add extension methods IsPalindrome

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            string lower = input.ToLower();
+
+            string res = new string(
+                lower.Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)).ToArray());
+
+            if (string.IsNullOrEmpty(res))
+                return false;
+
+            string revers = new string(res.Reverse().ToArray());
+    
+            return res == revers;
+        }
+    }
+}
