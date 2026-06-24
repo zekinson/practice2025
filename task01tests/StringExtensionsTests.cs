02b6cbe6e5c38117dba3b6e99107cf4ccc4d7819
zekinson <zekagorhakov@gmail.com>
2026-06-25 01:35:17 +0600

    test: add task01tests

---
+++ task01tests/StringExtensionsTests.cs
@@ -0,0 +1,45 @@
+using Xunit;
+using task01;
+
+public class StringExtensionsTests
+{
+    [Fact]
+    public void IsPalindrome_ValidPalindrome_ReturnsTrue()
+    {
+        string input = "А роза упала на лапу Азора";
+        Assert.True(input.IsPalindrome());
+    }
+
+    [Fact]
+    public void IsPalindrome_NotPalindrome_ReturnsFalse()
+    {
+        string input = "Hello, world!";
+        Assert.False(input.IsPalindrome());
+    }
+
+    [Fact]
+    public void IsPalindrome_EmptyString_ReturnsFalse()
+    {
+        string input = "";
+        Assert.False(input.IsPalindrome());
+    }
+
+    [Fact]
+    public void IsPalindrome_WithPunctuation_IgnoresPunctuation_ReturnsTrue()
+    {
+        string input = "Was it a car or a cat I saw?";
+        Assert.True(input.IsPalindrome());
+    }
+
+    public void IsPalindrome_OneLetter_ReturnsTrue()
+    {
+        string input = "A";
+        Assert.True(input.IsPalindrome());
+    }
+
+    public void IsPalindrome_OneWord_ReturnsTrue()
+    {
+        string input = "Дед";
+        Assert.True(input.IsPalindrome());
+    }
+}
