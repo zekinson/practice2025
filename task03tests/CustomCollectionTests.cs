5d58e9036a09eaf48c5c41af51c7b84d0223a6b3
zekinson <zekagorhakov@gmail.com>
2026-06-26 00:20:19 +0600

    fix: Update task03tests

--- task03tests/CustomCollectionTests.cs
+++ task03tests/CustomCollectionTests.cs
@@ -1,10 +1,66 @@
-namespace task03tests;
+using Xunit;
+using task03;
 
-public class UnitTest1
+public class IteratorTests
 {
     [Fact]
-    public void Test1()
+    public void CustomCollection_GetEnumerator_ReturnsAllItems()
     {
+        var collection = new CustomCollection<int>();
+        collection.Add(1);
+        collection.Add(2);
 
+        var result = new List<int>();
+        foreach (var item in collection)
+        {
+            result.Add(item);
+        }
+
+        Assert.Equal(new[] { 1, 2 }, result);
+    }
+
+    [Fact]
+        public void RemoveItem_ReturnsRightItems()
+        {
+            var collection = new CustomCollection<int>();
+            collection.Add(1);
+            collection.Add(2);
+            
+            bool removed = collection.Remove(1);
+            
+            Assert.True(removed);
+            var result = collection.ToList();
+            Assert.DoesNotContain(1, result);
+            Assert.Contains(2, result);
+        }
+
+    [Fact]
+    public void GetReverseEnumerator_ReturnsItemsInReverseOrder()
+    {
+        var collection = new CustomCollection<int>();
+        collection.Add(1);
+        collection.Add(2);
+
+        var result = collection.GetReverseEnumerator().ToList();
+        Assert.Equal(new[] { 2, 1 }, result);
+    }
+
+    [Fact]
+    public void GenerateSequence_ReturnsCorrectSequence()
+    {
+        var sequence = CustomCollection<int>.GenerateSequence(5, 3).ToList();
+        Assert.Equal(new[] { 5, 6, 7 }, sequence);
+    }
+
+    [Fact]
+    public void FilterAndSort_ReturnsFilteredAndSortedItems()
+    {
+        var collection = new CustomCollection<int>();
+        collection.Add(3);
+        collection.Add(1);
+        collection.Add(2);
+
+        var result = collection.FilterAndSort(x => x > 1, x => x).ToList();
+        Assert.Equal(new[] { 2, 3 }, result);
     }
 }
