ee98851b3a773765e76cab00b5041e4823d90029
zekinson <zekagorhakov@gmail.com>
2026-06-25 16:24:22 +0600

    feat: add realization task02

---
+++ task02/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace task02
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public List<int> Grades { get; set; }
+    }
+}
