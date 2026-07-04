f974d557f7931ce71d0c1d7442eb4ba316bb162c
zekinson <zekagorhakov@gmail.com>
2026-07-05 01:55:48 +0600

    feat: add plagins PlaginA, PlaginB, PlaginC

---
+++ PluginB/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PluginLib;
+
+namespace PluginB
+{
+    [PluginLoad("PluginB", "PluginA")]
+    public class PluginB
+    {
+        public void Execute()
+        {
+            Console.WriteLine("PluginB");
+        }
+    }
+}
