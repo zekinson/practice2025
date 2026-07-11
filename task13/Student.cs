9173e8796d61ef4fbc8b0daaa5fe9cac084298e9
zekinson <zekagorhakov@gmail.com>
2026-07-11 23:43:45 +0600

    feat: add Student.cs

---
+++ task13/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task13
+{
+    public class Subject
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        public Subject() { }
+
+        public Subject(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+    }
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public List<Subject> Grades { get; set; }
+
+        public Student() { }
+
+        public Student(string firstName, string lastName, DateTime birthDate, List<Subject> grades)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Grades = grades ?? new List<Subject>();
+        }
+    }
+}
